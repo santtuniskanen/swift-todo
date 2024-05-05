95db8639e98fa9dbbd88e1bfab2ddfcdd029ec04
santtuniskanen <santtuniska02@gmail.com>
2024-05-05 11:40:07 +0300

    init project

---
+++ TodoApplication/TodoApplication/TodoApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApplicationApp.swift
+//  TodoApplication
+//
+//  Created by santtuniskanen on 5.5.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
